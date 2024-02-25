fc43b6b0cea540a90a69869705e44d63d49477d0
Valeria-117 <154911862+Valeria-117@users.noreply.github.com>
2024-02-26 00:08:16 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+URL_SERVICE = 'https://49952359-6c39-4077-98a1-897e51e31f11.serverhub.praktikum-services.ru'
+ORDER_PATH = '/api/v1/orders'
+ORDER_TRACK_DATA_PATH = "/api/v1/orders/track?t="
+DOC_PATH = "/docs/"
