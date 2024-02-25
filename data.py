40df13e380e8c7b9675c538a621b3e632c82c2fd
Valeria-117 <154911862+Valeria-117@users.noreply.github.com>
2024-02-26 00:07:44 +0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Гарри",
+    "lastName": "Поттер",
+    "address": "Тисовая улица",
+    "metroStation": 4,
+    "phone": "+7 800 555 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2023-06-10",
+    "comment": "Чаевые десять галлеонов",
+    "color": [
+        "BLACK"
+    ]
+}
